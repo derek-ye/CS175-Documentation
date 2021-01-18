58fc10256e1a82c7ce183803adc0d8f32c230cb2
derek-ye <37666121+derek-ye@users.noreply.github.com>
2021-01-18 00:46:35 -0800

    deploy

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -7,7 +7,7 @@ set -e
 npm run build
 
 # navigate into the build output directory
-cd docs/.vuepress/dist
+cd src/.vuepress/dist
 
 # if you are deploying to a custom domain
 # echo 'www.example.com' > CNAME
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:<derek-ye>/<derek-ye>.github.io.git master
+git push -f git@github.com:derek-ye/derek-ye.github.io.git main
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
-cd -
+# cd -
